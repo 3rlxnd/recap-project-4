b6f9f37ae9a7c14356e92157c59c5770a885911b
3rlxnd <erlnd.gdt@gmail.com>
2025-01-22 17:45:18 +0100

    Solved #2 Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
   console.log("Find Issue 1");
+  const [ colors, setColors ] =useState(initialColors)
+
+  function handleAdd(newColor) {
+    setColors((prev) => [{
+      id: uid(), 
+      role: newColor.role,
+      hex: newColor.hex, 
+      contrastText: newColor.contrast 
+    }, ...prev])
+  }
+
+  
+
+
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
+      <ColorForm handleAdd={handleAdd}/>
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
