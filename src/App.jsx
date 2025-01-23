e92428ee077919032d94a0831447b52c4b8d6d51
3rlxnd <erlnd.gdt@gmail.com>
2025-01-23 09:53:43 +0100

    Solved #4 Issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,28 @@ import { uid } from "uid";
 
 function App() {
   console.log("Find Issue 1");
-  const [ colors, setColors ] =useState(initialColors)
+  const [colors, setColors] = useState(initialColors)
 
-  function handleAdd(newColor) {
-    
+  function handleAdd(newColor, id) {
     setColors((prev) => [{
-      id: uid(), 
+      id: uid(),
       role: newColor.role,
-      hex: newColor.hex, 
-      contrastText: newColor.contrast 
+      hex: newColor.hex,
+      contrastText: newColor.contrast
     }, ...prev])
   }
 
-  
+  function handleEdit(newColor, id) {
+    setColors((prev) =>
+      prev.map((color) => color.id === id ? {
+            ...color,
+            role: newColor.role,
+            hex: newColor.hex,
+            contrastText: newColor.contrast,
+          } : color
+      )
+    );
+  }
 
   function handleDelete(id) {
     setColors(colors.filter((color) => color.id !== id))
@@ -28,9 +37,9 @@ function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm handleAdd={handleAdd}/>
+      <ColorForm handleData={handleAdd} text='Add' values={{ contrast: '#ffffff', hex: '#000000', role: 'Primary Color' }} />
       {colors.length > 0 ? colors.map((color) => {
-        return <Color key={color.id} id={color.id} color={color} onDelete={handleDelete}/>
+        return <Color key={color.id} color={color} onDelete={handleDelete} onEdit={handleEdit} />
       }) : <p>Nothing here yet! Please add a Color</p>}
     </>
   );
--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react'
 import ColorInput from '../ColorInput/ColorInput'
 
-export default function ColorForm({handleAdd}) {
+export default function ColorForm({handleData, text, values, id}) {
+    const [role, setRole] = useState(values.role)
     
     function handleSubmit(event) {
         event.preventDefault()
         const formdata = new FormData(event.target)
         const data = Object.fromEntries(formdata)
-        handleAdd(data)
+        handleData({...data, role: role}, id)
+    }
+
+    function handleChange(event) {
+        setRole(event.target.value);
     }
 
     return (
         <form onSubmit={handleSubmit}>
             Role
-            <input id='role' type="text" name='role'/>
+            <input id='role' type="text" name='role' value={role} onChange={handleChange}/>
             <br />
             Hex
-            <ColorInput name='hex' value={'#ffffff'}/>
+            <ColorInput name='hex' value={values.hex}/>
             <br />
             Contrast Text
-            <ColorInput name='contrast' value={'#000000'}/>
-            <button >Add Color</button>
+            <ColorInput name='contrast' value={values.contrast}/>
+            <button >{text} Color</button>
         </form>
     )
 }
